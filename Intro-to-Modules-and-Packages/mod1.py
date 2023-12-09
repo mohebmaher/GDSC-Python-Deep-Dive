699a6bb0ab8d8d4c05f0d19d89bf67ce35a52f98
moheb_maher <dvm.moheb.maher@gmail.com>
2023-12-10 01:35:51 +0200

    Create mod1.py

---
+++ Intro-to-Modules-and-Packages/mod1.py
@@ -0,0 +1,16 @@
+print('Running the first module!')
+s1 = 'This is the first module.'
+a = 1
+_b = 2
+
+
+def foo():
+    print("foo")
+
+
+class Bar:
+    pass
+
+
+if (__name__ == '__main__'):
+    print(f'[__name__]')
